d13bce2bbadbac32a4293048d7116e8884721030
@priyanshu-vish <priyanshuvsk20@gmail.com>
2024-10-09 09:59:08 +0000

    change

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -15,7 +15,7 @@ export class AboutComponent {
       path : "https://static.wixstatic.com/media/2feeec_10bf81bc3e394f0d8daaaf5c62e15d6c~mv2.jpg/v1/fill/w_581,h_825,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/2feeec_10bf81bc3e394f0d8daaaf5c62e15d6c~mv2.jpg",
       h1 : "About",
       h2  : "Our School",
-      p : "I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font."
+      p : "Our school creates a supportive environment that encourages academic achievement, creativity, and community, helping students grow into lifelong learners and leaders."
     }
 
     staffDatas = [
--- src/app/admission/admission.component.ts
+++ src/app/admission/admission.component.ts
@@ -13,7 +13,7 @@ export class AdmissionComponent {
       path : "https://static.wixstatic.com/media/2feeec_54cc94e5673243a4843eafb567842e1d~mv2.jpg/v1/fill/w_547,h_743,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/2feeec_54cc94e5673243a4843eafb567842e1d~mv2.jpg",
       h1 : "Admissions",
       h2  : "",
-      p : "I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font.I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or  double click me to add your own content and make changes to the font.I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font."
+      p : "We nurture a love for learning in a supportive and inclusive environment. Our dedicated staff promotes academic excellence while fostering character development, respect, and collaboration. We celebrate diversity and encourage students to embrace challenges. With a dynamic curriculum that includes rigorous academics and diverse extracurricular activities, [School Name] prepares students to thrive in a changing world, helping them reach their full potential as responsible global citizens."
     }
   
 }
